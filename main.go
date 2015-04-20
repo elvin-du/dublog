b379c74ccfdbe71ef4ca2169ac6a74d5498ea2e5
nick Du <nick@yun.io>
2015-04-20 11:41:52 +0800

    auto open web browser

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 	"dublog/service"
 	"log"
 	"net/http"
+	"os/exec"
+	"runtime"
+	"time"
+    "errors"
 )
 
 var addr string
@@ -20,8 +24,36 @@ func init() {
 
 func main() {
 	service.Start()
+    go StartWeb()
 	err := http.ListenAndServe(addr, router.Router())
 	if nil != err {
 		log.Fatalln(err)
 	}
+}
+
+func StartWeb() {
+	time.Sleep(time.Second * 1)
+
+	err := Open("http://"+addr + "/home")
+	if nil != err {
+		log.Println(err)
+	}
+    log.Println("open ok")
+}
+
+var commands = map[string]string{
+	"windows": "start",
+	"darwin":  "open",
+	"linux":   "xdg-open",
+}
+
+func Open(uri string) error {
+	run, ok := commands[runtime.GOOS]
+	if !ok {
+		log.Printf("don't know how to open things on %s platform", runtime.GOOS)
+		return errors.New("cannot find os platform")
+	}
+
+	cmd := exec.Command(run, uri)
+	return cmd.Start()
 }
